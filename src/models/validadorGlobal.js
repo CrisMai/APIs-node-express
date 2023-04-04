d7c73e21819e98121cfdaa0bde83431362494f70
unknown <crismai.dev@gmail.com>
2023-04-04 14:07:18 -0300

    Filtrando por número de páginas

--- src/models/validadorGlobal.js
+++ src/models/validadorGlobal.js
@@ -4,5 +4,5 @@ import mongoose from "mongoose";
 mongoose.Schema.Types.String.set("validade", {
     validator: (valor) => valor !== "",
     message: ({path}) => `O campo ${path} foi fornecido em branco.`
-    
+
 });
